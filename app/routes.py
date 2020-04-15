f2f9c627637451adfe037ac8196b0b915915e19e
Ranjan21 <abhishek.ranjan@aeris.net>
2020-04-15 18:43:07 +0530

    adding India Status

--- app/routes.py
+++ app/routes.py
@@ -45,18 +45,38 @@ def wiki():
 def india():
     """
     Render the INDIA template on the /CoronaFacts route
-    
-    url2 = requests.get('https://www.covid19india.org/').text
-    soup2 = BeautifulSoup(url2, 'html.parser')
-
-    total_confirmed_cases = soup2.find("div", {"class": "level"})[3].text
-    total_death = soup2.find("h1").text
-    total_recovered = soup2.find_all("p")[9].text
     """
+    state = []
+    confirmed = []
+    death = []
+    url2 = requests.get('https://www.mohfw.gov.in/').text
+    soup = BeautifulSoup(url2, 'html.parser')
 
+    total_confirmed_cases = soup.find_all("strong")[6].text
+    total_death = soup.find_all("strong")[8].text
+    total_recovered = soup.find_all("strong")[7].text
 
-    return render_template('india.html')
+    #Parse data that are stored between <tr>...</tr>  
 
+    table = soup.find('table', {'class':'table table-striped'})
+    thead = table.findAll('thead')
+    head = [heading.text.strip() for heading in thead]
 
-
+    
+    if (head[1:4] == ['S. No., Name of State / UT, Total Confirmed cases (Including 76 foreign Nationals) ,Cured/Discharged/Migrated, Death']):
+        pass  
+    for table_row in table.findAll('tr'):
+        table_data = (table_row.find_all('td'))
+        if not table_data:
+            continue
+        
+        name = [tbd.text for tbd in table_data[1:2]]
+        confirm = [tbd.text for tbd in table_data[2:3]]
+        dead = [tbd.text for tbd in table_data[4:5]]
+        state.append(' '.join(name))
+        confirmed.append(' '.join(confirm))
+        death.append(' '.join(dead))
+        
+            
+    return render_template('india.html',total_confirmed_cases=total_confirmed_cases,total_death=total_death,total_recovered=total_recovered,confirmed=confirmed,state=state,death=death,table_data=table_data)
 
